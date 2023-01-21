00a0287673ff4586eab48826a5cd348c434ed706
cloobTech <belkid98@gmail.com>
2023-01-21 02:43:46 -0800

    fixed

--- sort.h
+++ sort.h
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 
 } listint_t;
-void print_array(const int* array, size_t size);
+void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
