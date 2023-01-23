b438a499387f3911e1fb444f4b825a0b3a9ab4d1
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-01-24 00:29:11 +0100

    implement selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+#include "swap.c"
+/**
+* selection_sort - sort an array using the selction sort method
+*@array: array to sort
+*@size: size of the array
+*Return: nothing
+*/
+
+void selection_sort(int *array, size_t size)
+{
+    size_t min, i, j;
+
+    if (!array)
+        return;
+
+    /*loop through each element in the array*/
+    for (i = 0; i < size - 1; i++)
+    {
+        /*assume the minimum is at index i*/
+        min = i;
+        /*move through list by comparing each element with element at i*/
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min])
+            {
+                min = j;
+
+            }
+
+        }
+        if (min != i)
+        {
+            swap(&array[i], &array[min]);
+        }
+
+        print_array(array, size);
+    }
+
+
+
+
+}
---
+++ swap.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+/**
+*swap - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
+*/
+
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+
+
+}
