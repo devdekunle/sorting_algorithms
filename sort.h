8458e2a1a7fa3bfa320304b40cb933592ce32045
cloobTech <belkid98@gmail.com>
2023-01-23 05:24:55 -0800

    sorted

--- sort.h
+++ sort.h
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+
 typedef struct listint_s
 {
 	const int n;
