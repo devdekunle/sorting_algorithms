8ddf07e1fa9bf3cbeb7aeec53406fd02971f2da5
cloobTech <belkid98@gmail.com>
2023-01-24 20:30:09 -0800

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 #include "swap.c"
+
 /**
 * selection_sort - sort an array using the selction sort method
 *@array: array to sort
@@ -9,28 +10,24 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t min, i, j;
+	size_t min, i, j;
 
-    if (!array)
-        return;
+	if (!array)
+		return;
 
-    /*loop through each element in the array*/
-    for (i = 0; i < size - 1; i++)
-    {
-        /*assume the minimum is at index i*/
-        min = i;
-        /*move through list by comparing each element with element at i*/
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-            {
-                min = j;
-            }
-        }
-        if (min != i)
-        {
-            swap(&array[i], &array[min]);
-        	print_array(array, size);
-        }
-    }
+	/*loop through each element in the array*/
+	for (i = 0; i < size - 1; i++)
+	{
+		/*assume the minimum is at index i*/
+		min = i;
+		/*move through list by comparing each element with element at i*/
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min])
+				min = j;
+		if (min != i)
+		{
+			swap(&array[i], &array[min]);
+			print_array(array, size);
+		}
+	}
 }
